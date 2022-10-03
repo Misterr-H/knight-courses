d28c18e147c88db64ca2e842d99d73b58146f00d
Mister-H <hs913271@gmail.com>
2022-10-03 18:23:14 +0530

    home page and course card added

--- pages/api/models/connection.js
+++ pages/api/models/connection.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 
-const connection = async () => mongoose.connect("mongodb://localhost/knightDB",
+const connection = async () => mongoose.connect(process.env.MONGO_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
